8294acadb6382f7d2d40423a417cda3df070f748
Juano <juanosierra2@gmail.com>
2021-09-01 17:33:17 -0500

    Mas pruebas
    Mas pruebas

--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/Registry.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/Registry.java
@@ -2,9 +2,16 @@ package edu.eci.cvds.tdd.registry;
 
 public class Registry {
     public RegisterResult registerVoter(Person p) {
+        if(p.isAlive() == false){
+            return RegisterResult.DEAD;
+        }
+        else if(p.getAge() < 18){
+            return RegisterResult.UNDERAGE;
+        }
+        else if(p.getAge() <0 || p.getAge() > 120){
+            return RegisterResult.INVALID_AGE;
+        }
 
-        // TODO Validate person and return real result.
         return RegisterResult.VALID;
     }
 }
-
--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
@@ -10,27 +10,28 @@ public class RegistryTest {
     @Test
     public void validateRegistryResult() {
 
-        Person person = new Person();
+        Person person = new Person("Bruce",3,99,Gender.FEMALE,true);
+
         RegisterResult result = registry.registerVoter(person);
+
         Assert.assertEquals(RegisterResult.VALID, result);
     }
     @Test
-    public void validateAliveVoter(){
-	Person person = new Person();
+    public void validateisAlivepeople(){
+        Person person = new Person("cesar",1,19,Gender.MALE,false);
+
         RegisterResult result = registry.registerVoter(person);
-	Assert.assertTrue(result != RegisterResult.DEAD);
+
+        Assert.assertEquals(RegisterResult.DEAD, result);
     }
+
     @Test
-    public void validateValidEdge(){		
-        Person person = new Person();
-        RegisterResult result = registry.registerVoter(person);
-	Assert.assertTrue(result != RegisterResult.INVALID_AGE);
-    }
-    @Test
-    public void validateAdult(){
-        Person person = new Person();
-        RegisterResult result = registry.registerVoter(person);
-	Assert.assertTrue(result != RegisterResult.UNDERAGE);
+    public void validateisUnderAge(){
+        Person person1 = new Person("Camila",2,17,Gender.FEMALE,true);
+
+        RegisterResult result = registry.registerVoter(person1);
+
+        Assert.assertEquals(RegisterResult.UNDERAGE, result);
     }
 
     // TODO Complete with more test cases
